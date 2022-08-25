e1ca515f9c8290b3b77eab553121ebb4ace70d46
Michae Bett <bett.mike@yahoo.com>
2022-08-25 19:19:48 +0800

    fixed flask

--- Machine_Learning_Model/FlaskApp.py
+++ Machine_Learning_Model/FlaskApp.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # Load the pickle model
 
-model = pickle.load(open("finalized_model.pkl", "rb"))
+#model = pickle.load(open("finalized_model.pkl", "rb"))
 
 
 #################################################
